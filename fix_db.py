fd6982188dc55f0223240814c5028b3cc3a83c49
Frankez75 <chidubem.frank@yahoo.com>
2026-09-16 15:22:15 +0100

    Remove local utility files from tracking

--- fix_db.py
+++
@@ -1,7 +0,0 @@
-from run import app
-from pkg.models import db
-
-with app.app_context():
-    db.session.execute(db.text("DROP TABLE IF EXISTS alembic_version"))
-    db.session.commit()
-    print("Dropped alembic_version table successfully.")
